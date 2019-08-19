50ad38c6d62fa0808e3c8fcec89104540654c29b
David Cunningham <david@dcdominion.com>
2019-08-19 12:19:20 -0400

    Update setuposx.sh

--- setuposx.sh
+++ setuposx.sh
@@ -13,11 +13,14 @@ else
 fi
 
 # Install or update brew packages
-brew tap homebrew/bundle
-brew bundle
+#brew tap homebrew/bundle
+#brew bundle
 
 # Install node packages
-which -s yo || npm install -g yo
-which -s bower || npm install -g bower
-which -s grunt || npm install -g grunt-cli
-which -s gulp  || npm install -g gulp
+#which -s yo || npm install -g yo
+#which -s bower || npm install -g bower
+#which -s grunt || npm install -g grunt-cli
+#which -s gulp  || npm install -g gulp
+
+# Install XCode command line tools
+xcode-select --install
